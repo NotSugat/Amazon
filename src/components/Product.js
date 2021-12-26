b0bf67fec2e7cd212e1449c89b235e6ff9598c62
NotSugat <sugatsujakhu@gmail.com>
2021-12-26 20:45:24 +0545

    new change

--- src/components/Product.js
+++ src/components/Product.js
@@ -14,11 +14,20 @@ function Product({ id, title, price, description, category, image }) {
 
   return (
     <div className=' relative flex flex-col m-5 bg-white z-30 p-10 '>
-      <p className='absolute  top-2 right-2 text-xs italic text-gray-400'>
+      <p
+        key={id}
+        className='absolute  top-2 right-2 text-xs italic text-gray-400'
+      >
         {category}
       </p>
 
-      <Image src={image} height={200} width={200} objectFit='contain' />
+      <Image
+        key={id}
+        src={image}
+        height={200}
+        width={200}
+        objectFit='contain'
+      />
 
       <h4 className='my-3 font-semibold'>{title}</h4>
 
@@ -26,13 +35,15 @@ function Product({ id, title, price, description, category, image }) {
         {Array(rating)
           .fill()
           .map((_, i) => (
-            <StarIcon className='h-5' />
+            <StarIcon key={id} className='h-5' />
           ))}
       </div>
 
-      <p className='text-xs my-2 line-clamp-2 '>{description}</p>
+      <p key={id} className='text-xs my-2 line-clamp-2 '>
+        {description}
+      </p>
       <div className=' mb-5'>
-        <p>${price}</p>
+        <p key={id}>${price}</p>
       </div>
       {hasPrime && (
         <div className='flex items-center space-x-2 -mt-5'>
